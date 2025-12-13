4212ffcfae831e79613d88f2496a8ba2af9a5a57
Abdullah Alshameri <abdullah.ashameri01@gmail.com>
2025-12-13 17:14:14 +0300

    Categories - Manage Page New Idea

--- admin/categories.php
+++ admin/categories.php
@@ -35,7 +35,8 @@ if (isset($_SESSION['Username'])) {
                         foreach($cats as $cat) {
                             echo "<div class='cat'>";
                                 echo "<div class='hidden-buttons'>";
-                                    echo "<a href='#' class='btn btn-xs btn-primary'><i class='fa fa-edit'></a>Edit</a>";
+                                    echo "<a href='#' class='btn btn-xs btn-primary'><i class='fa fa-edit'></i>Edit</a>";
+                                    echo "<a href='#' class='btn btn-xs btn-danger'><i class='fa fa-close'></i>Delete</a>";
                                 echo "</div>";
                                 echo "<h3>" . $cat['Name'] . '</h3>';
                                 echo "<p>"; if($cat['Description'] == '') {echo 'This category has no description'; } else {echo $cat['Description'];} echo"</p>";
