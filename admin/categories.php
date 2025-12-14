bea2289795705a74d257e14235b14725409f6786
Abdullah Alshameri <abdullah.ashameri01@gmail.com>
2025-12-14 18:18:23 +0300

    Categories - Create Edit Page

--- admin/categories.php
+++ admin/categories.php
@@ -29,13 +29,11 @@ if (isset($_SESSION['Username'])) {
         if (isset($_GET['type']) && in_array($_GET['type'], $type_array)) {
 
             $type = $_GET['type'];
-
         }
 
         if (isset($_GET['sort']) && in_array($_GET['sort'], $sort_array)) {
 
             $sort = $_GET['sort'];
-
         }
 
         $stmt2 = $con->prepare("SELECT * FROM categories ORDER BY $type $sort");
@@ -50,35 +48,54 @@ if (isset($_SESSION['Username'])) {
                 <div class="panel-heading">
                     Manage Categories
                     <div class="ordering pull-right">
-                    Ordering:
-                    <a class="<?php if($sort == 'ASC') { echo 'active'; } ?>" href="?type=<?php echo $type; ?>&sort=ASC">Asc</a> | 
-                    <a class="<?php if($sort == 'DESC') { echo 'active'; } ?>" href="?type=<?php echo $type; ?>&sort=DESC">Desc</a>  
-                </div>
+                        Ordering:
+                        <a class="<?php if ($sort == 'ASC') {
+                                        echo 'active';
+                                    } ?>" href="?type=<?php echo $type; ?>&sort=ASC">Asc</a> |
+                        <a class="<?php if ($sort == 'DESC') {
+                                        echo 'active';
+                                    } ?>" href="?type=<?php echo $type; ?>&sort=DESC">Desc</a>
+                    </div>
 
-                <div class="ordering pull-right">
-                    Type:
-                    <a class="<?php if($type == 'Ordering') { echo 'active'; } ?>" href="?type=Ordering&sort=<?php echo $sort; ?>">Ordering</a> | 
-                    <a class="<?php if($type == 'ID') { echo 'active'; } ?>" href="?type=ID&sort=<?php echo $sort; ?>">ID</a> | 
-                </div>
-
+                    <div class="ordering pull-right">
+                        By:
+                        <a class="<?php if ($type == 'Ordering') {
+                                        echo 'active';
+                                    } ?>" href="?type=Ordering&sort=<?php echo $sort; ?>">Ordering</a> |
+                        <a class="<?php if ($type == 'ID') {
+                                        echo 'active';
+                                    } ?>" href="?type=ID&sort=<?php echo $sort; ?>">ID</a> |
+                    </div>
                 </div>
                 <div class="panel-body">
-                    <?php   
-                        foreach($cats as $cat) {
-                            echo "<div class='cat'>";
-                                echo "<div class='hidden-buttons'>";
-                                    echo "<a href='#' class='btn btn-xs btn-primary'><i class='fa fa-edit'></i>Edit</a>";
-                                    echo "<a href='#' class='btn btn-xs btn-danger'><i class='fa fa-close'></i>Delete</a>";
-                                echo "</div>";
-                                echo "<h3>" . $cat['Name'] . '</h3>';
-                                echo "<p>"; if($cat['Description'] == '') {echo 'This category has no description'; } else {echo $cat['Description'];} echo"</p>";
-                                echo 'Ordering Is ' . $cat['Ordering'] . '<br />';
-                                if($cat['Visibility'] == 1) {echo '<span class="visibility">Hidden</span>'; }
-                                if($cat['Allow_Comment'] == 1) {echo '<span class="commenting">Comment Disabled</span>'; }
-                                if($cat['Allow_Ads'] == 1) {echo '<span class="advertises">Ads Disabled</span>'; }
-                            echo "</div>";
-                            echo "<hr>";
+                    <?php
+                    foreach ($cats as $cat) {
+                        echo "<div class='cat'>";
+                        echo "<div class='hidden-buttons'>";
+                        echo "<a href='categories.php?do=Edit&catid=" . $cat['ID'] . "' class='btn btn-xs btn-primary'><i class='fa fa-edit'></i>Edit</a>";
+                        echo "<a href='#' class='btn btn-xs btn-danger'><i class='fa fa-close'></i>Delete</a>";
+                        echo "</div>";
+                        echo "<h3>" . $cat['Name'] . '</h3>';
+                        echo "<p>";
+                        if ($cat['Description'] == '') {
+                            echo 'This category has no description';
+                        } else {
+                            echo $cat['Description'];
                         }
+                        echo "</p>";
+                        echo 'Ordering Is ' . $cat['Ordering'] . '<br />';
+                        if ($cat['Visibility'] == 1) {
+                            echo '<span class="visibility">Hidden</span>';
+                        }
+                        if ($cat['Allow_Comment'] == 1) {
+                            echo '<span class="commenting">Comment Disabled</span>';
+                        }
+                        if ($cat['Allow_Ads'] == 1) {
+                            echo '<span class="advertises">Ads Disabled</span>';
+                        }
+                        echo "</div>";
+                        echo "<hr>";
+                    }
                     ?>
                 </div>
             </div>
@@ -168,7 +185,7 @@ if (isset($_SESSION['Username'])) {
             </form>
         </div>
 
-<?php
+        <?php
 
     } elseif ($do == 'Insert') {
 
@@ -228,7 +245,121 @@ if (isset($_SESSION['Username'])) {
             echo "</div>";
         }
     } elseif ($do == 'Edit') {
+        // Check if Get Request catid Is Numeric & Get The Integer Value Of It
+
+        $catid = isset($_GET['catid']) && is_numeric($_GET['catid']) ? intval($_GET['catid']) : 0;
+
+        //Select All Data Depend In This ID
+        $stmt = $con->prepare("SELECT * FROM categories WHERE ID = ?");
+
+        // Execute
+        $stmt->execute(array($catid));
+
+        // Fetch The Data
+        $cat = $stmt->fetch();
+
+        // The Row Count
+        $count = $stmt->rowCount();
+
+        // If There is Such Id Show The Form
+        if ($count > 0) { ?>
+
+            <h1 class="text-center">Edit Category</h1>
+            <div class="container">
+                <form class="form-horizontal" action="?do=Update" method="POST">
+                    <input type="hidden" name="catid" value="<?php echo $catid ?>" />
+                    <!-- Starat Name Field -->
+                    <div class="form-group form-group-lg">
+                        <label class="col-sm-2 control-label">Name</label>
+                        <div class="col-sm-10 col-md-6">
+                            <input type="text" name="name" class="form-control" required="required" placeholder="Name Of The Category" value="<?php echo $cat['Name']; ?>" />
+                        </div>
+                    </div>
+                    <!-- End Name Field -->
+                    <!-- Starat Description Field -->
+                    <div class="form-group form-group-lg">
+                        <label class="col-sm-2 control-label">Description</label>
+                        <div class="col-sm-10 col-md-6 password-box">
+                            <input type="text" name="description" class="form-control" placeholder="Describe The Category" value="<?php echo $cat['Description']; ?>" />
+                        </div>
+                    </div>
+                    <!-- End Description Field -->
+                    <!-- Starat Ordering Field -->
+                    <div class="form-group form-group-lg">
+                        <label class="col-sm-2 control-label">Ordering</label>
+                        <div class="col-sm-10 col-md-6">
+                            <input type="text" name="ordering" class="form-control" placeholder="Number To Array The Categories" value="<?php echo $cat['Ordering'] ?>" />
+                        </div>
+                    </div>
+                    <!-- End Ordering Field -->
+                    <!-- Starat Visibility Field -->
+                    <div class="form-group form-group-lg">
+                        <label class="col-sm-2 control-label">Visible</label>
+                        <div class="col-sm-10 col-md-6">
+                            <div>
+                                <input id="vis-yes" type="radio" name="visibility" value="0" <?php if ($cat['Visibility'] == 0 ) {echo 'checked'; }?> />
+                                <label for="vis-yes">Yeas</label>
+                            </div>
+                            <div>
+                                <input id="vis-no" type="radio" name="visibility" value="1" <?php if ($cat['Visibility'] == 1 ) {echo 'checked'; }?> />
+                                <label for="vis-no">No</label>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Visibility Field -->
+                    <!-- Starat Commenting Field -->
+                    <div class="form-group form-group-lg">
+                        <label class="col-sm-2 control-label">Allow Commenting</label>
+                        <div class="col-sm-10 col-md-6">
+                            <div>
+                                <input id="com-yes" type="radio" name="commenting" value="0" <?php if ($cat['Allow_Comment'] == 0) {echo 'checked'; }?> />
+                                <label for="com-yes">Yeas</label>
+                            </div>
+                            <div>
+                                <input id="com-no" type="radio" name="commenting" value="1" <?php if( $cat['Allow_Comment'] == 1) {echo 'checked'; }?> />
+                                <label for="com-no">No</label>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Commenting Field -->
+                    <!-- Starat Ads Field -->
+                    <div class="form-group form-group-lg">
+                        <label class="col-sm-2 control-label">Allow Ads</label>
+                        <div class="col-sm-10 col-md-6">
+                            <div>
+                                <input id="ads-yes" type="radio" name="ads" value="0" <?php if($cat['Allow_Ads'] == 0) {echo 'checked'; } ?> />
+                                <label for="ads-yes">Yeas</label>
+                            </div>
+                            <div>
+                                <input id="ads-no" type="radio" name="ads" value="1" <?php if($cat['Allow_Comment'] == 1) {echo 'checked'; }?> />
+                                <label for="ads-no">No</label>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Ads Field -->
+                    <!-- Starat Submit Field -->
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <input type="submit" value="Save" class="btn btn-primary btn-lg" />
+                        </div>
+                    </div>
+                    <!-- End Submit Field -->
+                </form>
+            </div>
+
+<?php
+            // If There's No Such ID Show Error Message
+        } else {
+
+            echo "<div class='container'>";
+            $theMsg = '<div class="alert alert-danger"> There is No Such ID </div>';
+
+            redirectHome($theMsg);
+
+            echo "<</div>";
+        }
     } elseif ($do == 'Update') {
+
     } elseif ($do == 'Delete') {
     }
 
